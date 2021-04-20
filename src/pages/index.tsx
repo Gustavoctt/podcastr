a51f333e3c5a2a4599541dbe5c91a2bb86185068
Gustavoctt <gustavocarrertartare@gmail.com>
2021-04-20 11:45:52 -0300

    Create API with SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
-export default function Home() {
+export default function Home(props) {
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+ 
+  return{
+    // Vai retornar as propriedades da minha requisição
+    props: {
+      episodes: data,
+    }, 
+    // Vai fazer uma nova requisição após 8 horas - Tempo em segundos
+    revalidate : 60 * 60 * 8,
+  }
 }
